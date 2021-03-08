a20892ef970bc0c43988fa8bd46a2e9f69685644
Sam Cook <samuel.j.cook@durham.ac.uk>
2021-03-08 18:33:48 +0000

    refactoring

--- gsgw38.py
+++ gsgw38.py
@@ -1,41 +1,40 @@
-# import numpy # for data manipulation
+#### IMPORTS ####
 import pandas as pd # for data manipulation and .describe()
-# import matplotlib # for visualisation
-# import sklearn # for model training
 
+#### READ IN DATA ####
+cov = pd.read_csv("datasets/latestdata.csv") # original (can be switched out for smaller or outcome to run quicker)
 
-# load the data
-# cov = pd.read_csv("datasets/latestdata.csv") # original
-# cov = pd.read_csv("datasets/smaller.csv") # without the irrelevant columns
-# cov = pd.read_csv("datasets/outcome.csv") # only rows with an outcome entry (307382)
-
+# Inspect data
 # print(cov.count()) # print(cov["sex"].describe() # also good
 
-# Ideally I"d like to do this
-fields = ["age", "sex", "outcome", "date_admission_hospital", "date_onset_symptoms", "country"] # 228 rows
-
+#### SELECT ONLY THE RELEVANT COLUMNS ####
 # Interesting problems which don't have quite enough data
-fields = ["outcome", "date_admission_hospital", "date_onset_symptoms"] # 234 rows
-fields = ["outcome", "date_admission_hospital", "date_confirmation"] # 262 rows
-fields = ["outcome", "date_confirmation", "date_onset_symptoms"] # 3505 rows
+# fields = ["age", "sex", "outcome", "date_admission_hospital", "date_onset_symptoms", "country"] # 228 rows
+# fields = ["outcome", "date_admission_hospital", "date_onset_symptoms"] # 234 rows
+# fields = ["outcome", "date_admission_hospital", "date_confirmation"] # 262 rows
+# fields = ["outcome", "date_confirmation", "date_onset_symptoms"] # 3505 rows
 
-
-# The problem I intend to solve
+# Identify the columns required for the problem
 fields = ["outcome", "age", "sex", "date_confirmation", "date_onset_symptoms", "country"] # 3493 rows - mostly from the phillipines
 # Without the dates - worth having a look into
 # fields = ["outcome", "country", "age", "sex"] # 33599 rows
 
-# drop the columns which aren"t in the fields we want
-# dataset = cov[fields]
-# # drop the rows which are missing information
-# dataset = dataset.dropna(subset=fields)
-# # store the set
-# dataset.to_csv("datasets/dataset1.csv")
+# Select these columns from the dataset
+dataset = cov[fields]
 
+
+#### DATA CLEANING ####
+ 
+# Drop the rows which are missing information
+dataset = dataset.dropna(subset=fields)
+
+# Store the set for future use
+dataset.to_csv("datasets/dataset1.csv")
+# Load the dataset if not doing the earlier steps
 dataset = pd.read_csv('datasets/dataset1.csv')
 
-# Data cleaning
-# The outcome column contains a lot of dubious information so let's tidy it up.
+
+# The outcome column contains a lot of different variations on three values.
 dataset = dataset.replace(to_replace={'outcome': {
     'death':'died',
     'Deceased':'died',
@@ -51,6 +50,7 @@ dataset = dataset.replace(to_replace={'outcome': {
     'Alive':'recovered',
     }})
 
+# The ages are a mixture of numbers and ranges so we tidy these too
 dataset = dataset.replace(to_replace={'age': {
     '^[0-9]$':'0-9',
     '1[0-9][.-]*[0-9]{0,2}':'10-19',
@@ -64,10 +64,10 @@ dataset = dataset.replace(to_replace={'age': {
     '9[0-9][.-]*[0-9]{0,2}':'90-99',
     }}, regex=True)
 
-dataset['age'].value_counts()
-
+# Drop the unnamed column
 dataset = dataset.drop(columns=['Unnamed: 0'])
 
+# Calculate the time that patients were waiting and store this in a new column (replacing the previous dates)
 gaps = []
 from datetime import date, datetime, timedelta
 for i in range(len(dataset['date_confirmation'])):
@@ -76,50 +76,40 @@ for i in range(len(dataset['date_confirmation'])):
     gaps.append(dataset['date_confirmation'][i] - dataset['date_onset_symptoms'][i])
 
 dataset['days_waiting'] = gaps
-
 dataset = dataset.drop(columns=['date_confirmation', 'date_onset_symptoms'])
 
-dataset.to_csv('datasets/dataset1_clean_outcome.csv')
+# Save the cleaned up dataset for future use
+dataset.to_csv('datasets/dataset1_clean.csv')
 
-## encode features as integers
+
+#### DATA PREPARATION ####
+# Split the data into features and labels
 features = dataset[['days_waiting', 'age', 'sex', 'country']]
 labels = dataset['outcome']
 
+# One encode the features as integers
 features = features.replace(to_replace={'sex': {
     'male':0,
     'female':1
 }})
 features['days_waiting'] = features['days_waiting'].dt.days
 
-## return dataframes
-# features.drop(features.loc[features['country'] in ['Angola', 'Gambia', 'South Korea', 'Brazil', 'Canada','Gabon','Romania','Nepal','France','India','Germany','Cabo Verde','Central African Republic']].index, inplace=True)
+# Use one hot encoding for the age and the country # TODO IS THERE A BETTER WAY OF ENCODING THESE?
 countries_df = pd.get_dummies(features['country'])
 age_df = pd.get_dummies(features['age'])
 features = pd.concat([features, countries_df, age_df], axis=1)
 features = features.drop(columns=['country', 'age'])
 
+# Encode the labels with integers
 labels = labels.replace({
     'died':0,
     'hospitalized':1,
     'recovered':2
 })
 
-print(features.head)
-
-### Model training 
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.1, random_state=0)
-
-
-from sklearn import svm
-
-clf = svm.SVC()
-clf.fit(X_train, y_train)
-
-# print(clf.predict( [[15,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0]] ))
 
 def person_encode(days_waiting, sex, country, age):
+    '''Function to encode the features of a person in the form used for the model'''
     array = [days_waiting]
     if sex == 'male':
         array.append(0)
@@ -133,4 +123,21 @@ def person_encode(days_waiting, sex, country, age):
     array.extend([int(a == age) for a in ages])
     return array
 
+
+#### MODEL TRAINING ####
+# It may be necessary to sample the dataset to ensure a more balanced set of recovered/vs died results
+# Split the data into training and testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.1, random_state=0)
+
+
+# Import and fit a Support Vector Machine
+from sklearn import svm
+clf = svm.SVC()
+clf.fit(X_train, y_train)
+
+# TODO train 2 more types of model - naive bayes and logistic regression?
+
+#### MODEL EVALUATION
 print(clf.predict([ person_encode(22, 1, 'Japan', '80-89') ]))
+
